e6d5ff67f0ec28e8c849d7f4a74dd651f4640f56
Denis Popkov <pkvdenis@gmail.com>
2022-04-01 22:06:42 +0300

    Implement Router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
+  {path: 'profile', component: ProfileComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'detail/:id', component: DetailComponent},
+  
+  {path: '401', component: UnauthorizedComponent},
+  {path: '404', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
