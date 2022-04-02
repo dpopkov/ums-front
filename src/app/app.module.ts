f58448312853f89fa5ac23ff799a6991263ce0a4
Denis Popkov <pkvdenis@gmail.com>
2022-04-02 15:31:34 +0300

    Implement AuthenticationService - sign-in, sign-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  readonly API_URL = environment.BASE_URL + '/api/authentication';
+
+  public currentUser: Observable<User>;
+  private currentUserSubject: BehaviorSubject<User>;
+
+  constructor(private http: HttpClient) {
+    let storageUser;
+    const storageUserAsStr = localStorage.getItem('currentUser');
+    if (storageUserAsStr) {
+      storageUser = JSON.parse(storageUserAsStr);
+    }
+    this.currentUserSubject = new BehaviorSubject<User>(storageUser);
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(user: User): Observable<any> {
+    return this.http.post<User>(this.API_URL + '/sign-in', user).pipe(
+      map(response => {
+        if (response) {
+          this.setSessionUser(response);
+        }
+        return response;
+      })
+    );
+  }
+
+  private setSessionUser(user: User) {
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    this.currentUserSubject.next(user);
+  }
+
+  register(user: User): Observable<any> {
+    return this.http.post(this.API_URL + '/sign-up', user);
+  }
+
+  logOut() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(new User());
+  }
+
+  refreshToken() {
+    return this.http.post(this.API_URL + '/refresh-token?token=' + this.currentUserValue?.refreshTokenId, {});
+  }
+}
