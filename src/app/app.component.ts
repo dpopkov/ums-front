419e6c24f8872e30e2976035ca8527722340f3a6
Denis Popkov <pkvdenis@gmail.com>
2022-04-03 15:34:53 +0300

    Add current user to AppComponent and use it in Navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Role } from './models/role.enum';
+import { User } from './models/user.model';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'User Management System';
+
+  currentUser: User = new User();
+
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {        
+    this.authenticationService.currentUser.subscribe(
+      data => {
+        this.currentUser = data;
+      }
+    )
+  }
+
+  isAdmin(): boolean {
+    return this.currentUser?.role === Role.ADMIN;
+  }
+
+  currentUserIsUndefined(): boolean {
+    return !this.currentUser?.id;
+  }
+
+  logOut(): void {
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+  }
 }
