2437b1702c4c3477ce18b1876e43cd16ac0b9676
Denis Popkov <pkvdenis@gmail.com>
2022-04-03 14:44:12 +0300

    Implement Login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { User } from 'src/app/models/user.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  faUser = faUserCircle;
+  errorMessage: string = '';
+
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.currentUserValue?.id) {
+      this.router.navigate(['/profile']);
+    }
+  }
+
+  login() {
+    this.authenticationService.login(this.user).subscribe(
+      data => {
+        this.router.navigate(['/profile']);
+      },
+      error => {
+        this.errorMessage = 'Username or password is incorrect';
+        console.log('LoginComponent:login:error=', error);
+      }
+    )
   }
 
 }
